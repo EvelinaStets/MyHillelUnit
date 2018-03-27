ac53fa91d99c75b968914cc42e3fa3848bcde74a
EvelinaStets <eve4lina@gmail.com>
2018-03-27 21:09:01 +0300

    maven added

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,40 +1,75 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import java.io.IOException;
 
 public class MyTests {
-    public static void main(String[] args) throws IOException {
-        // String[] testData = { "0975156900", "(097)5156900", "(097)515-69-00", "(097)51-56-900", "+380975156900",
-        //         "+38 097 515 69 00" };
 
-        test1_isPhone();
-        test2_isPhone();
-    }
 
     public static boolean isPhone(String text) {
         String regex = "(?:\\+38)? ?(?:0\\d{2}|\\(0\\d{2}\\)) ?\\d{2}(?:\\d{3}|\\d[- ]\\d{2}[- ]|[ -]\\d{2}[ -]\\d)\\d{2}";
         return text.matches(regex);
     }
 
+    @Test
     public static void test1_isPhone() {
-        String testData = "0975156900";
+        String testData = "0971132645";
+
+        boolean result = isPhone(testData);
+
+        Assert.assertTrue(result, "test1 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
+
+    }
+
+    @Test
+    public static void test2_isPhone() {
+        String testData = "fhrftdgf";
+
+        boolean result = isPhone(testData);
+
+        Assert.assertFalse(result, "test2 failed");
+    }
+
+    @Test
+    public static void test3_isPhone() {
+        String testData = "+380631132645";
 
         boolean result = isPhone(testData);
 
         if (result == true) {
-            System.out.println("Test1 passed");
+            System.out.println("Test3 passed");
         } else {
-            System.out.println("Test1 failed");
+            Assert.assertTrue(result, "test3 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
+            //System.out.println("Test1 failed");
         }
     }
 
-    public static void test2_isPhone() {
-        String testData = "asdaddasds";
+    @Test
+    public static void test4_isPhone() {
+        String testData = "+38(063)113 26 45";
 
         boolean result = isPhone(testData);
 
-        if (result == false) {
-            System.out.println("Test2 passed");
+        if (result == true) {
+            System.out.println("Test4 passed");
         } else {
-            System.out.println("Test2 failed");
+            Assert.assertTrue(result, "test4 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
+            //System.out.println("Test1 failed");
+        }
+    }
+
+    @Test
+    public static void test5_isPhone() {
+        String testData = "+38(063)113 26 45";
+
+        boolean result = isPhone(testData);
+
+        if (result == true) {
+            System.out.println("Test5 passed");
+        } else {
+            Assert.assertTrue(result, "test5 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
+            //System.out.println("Test1 failed");
         }
     }
 }
+
