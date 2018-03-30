e59a5b87f2611d020f9d6c5b4223f7c9895f3025
Stets <EStets@kiev.luxoft.com>
2018-03-30 14:21:17 +0300

    test6 was added

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -70,6 +70,18 @@ public class MyTests {
             Assert.assertTrue(result, "test5 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
             //System.out.println("Test1 failed");
         }
+
+
+    }
+
+    @Test
+    public static void test6_isPhone() {
+        String testData = "063 My113-26-45";
+
+        boolean result = isPhone(testData);
+
+        Assert.assertTrue(result, "test6 failed");// если будет фолс, то вылетит ошибка, тест помещается как фейл, но тесты другие будут проганятсья дальше
+
     }
 }
 
